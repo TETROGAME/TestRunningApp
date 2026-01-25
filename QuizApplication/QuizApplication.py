7a8a368bb72ed88dc4baae8e785e15b6176b50f1
Nikita Timoshenko <minecrosoft231231@gmai.com>
2026-01-25 16:53:01 +0300

    Add UI construction

--- QuizApplication/QuizApplication.py
+++ QuizApplication/QuizApplication.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter.font import Font
 
 from TestRunner.TestRunner import TestRunner
 
@@ -7,9 +8,33 @@ from TestRunner.TestRunner import TestRunner
 class QuizApplication:
     test_runner: TestRunner
     def __init__(self, root: tk.Tk, test_runner: TestRunner) -> None:
+        self.current_index = 0
+
         self.test_runner = test_runner
+
         self.root = root
+        root.title('Программа тестирования "placeholder"')
+        self.__build_ui()
         self.__display_questions()
 
+    def __build_ui(self):
+        self.font = Font(
+            family='Arial',
+            size=20
+        )
+        self.title_label = tk.Label(self.root, text="Текст вопроса", font=self.font)
+        self.title_label.pack(pady=20)
+        self.options_frame = tk.Frame(self.root)
+        self.options_frame.pack(pady=20)
+        self.next_button = tk.Button(self.root, text="Следующий вопрос", font=self.font, command=self.__next_question)
+        self.next_button.pack(pady=20)
+        pass
+
     def __display_questions(self):
+        for widget in self.options_frame.winfo_children():
+            widget.destroy()
+
+        # current_question = self.test_runner.question_database[self.current_index]
+
+    def __next_question(self):
         pass
