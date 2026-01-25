20b52e380ec24539c1f61b0b7547eaae6c777f35
Nikita Timoshenko <minecrosoft231231@gmai.com>
2026-01-25 14:51:56 +0300

    Revert "Add dunder to question_database variable"
    This reverts commit d37f9d2d68bf702963e69e87fc2530f56d812331.

--- TestRunner/TestRunner.py
+++ TestRunner/TestRunner.py
@@ -4,17 +4,17 @@ from TestRunner.QuestionLoader import QuestionLoader
 
 
 class TestRunner:
-    __question_database: List[Question]
+    question_database: List[Question]
 
     def __init__(self, file_path: str):
         loader = QuestionLoader()
         try:
-            self.__question_database = loader.load(file_path)
+            self.question_database = loader.load(file_path)
         except ValueError as e:
             print(e)
 
     def validate_answer(self, question_id: int, user_answer_ids: List[int]) -> bool:
-        target = self.__question_database[question_id]
+        target = self.question_database[question_id]
         if target.correct_answer_ids == user_answer_ids:
             return True
         else:
