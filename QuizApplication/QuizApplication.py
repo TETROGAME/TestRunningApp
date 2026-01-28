73146f2878011cea963ecbc3859e68db1b4a7c26
Nikita Timoshenko <minecrosoft231231@gmai.com>
2026-01-28 15:16:57 +0300

    Add color indication for previously selected options show mistakes screen

--- QuizApplication/QuizApplication.py
+++ QuizApplication/QuizApplication.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter.font import Font
 
+from TestRunner.Question import Option
 from TestRunner.TestRunner import TestRunner
 
 
@@ -47,7 +48,6 @@ class QuizApplication:
 
 
     def __next_question(self) -> None:
-        question = self.test_runner.question_database[self.current_index]
         selected_option_ids = [
             opt_id
             for opt_id, var in zip(self.current_option_ids, self.user_choices)
@@ -56,7 +56,7 @@ class QuizApplication:
         self.test_runner.submit_answer(self.current_index, selected_option_ids)
 
         self.current_index += 1
-        if self.current_index < len(self.test_runner.question_database):
+        if self.current_index <= len(self.test_runner.question_database):
             self.__display_questions()
         else:
             self.__show_score()
@@ -75,8 +75,35 @@ class QuizApplication:
         )
         if option:
             self.__show_mistakes()
-        self.root.destroy()
 
+    def __is_checked(self, question_id: int, option: Option) -> bool:
+        return option.id in self.test_runner.user_answer_ids[question_id]
     def __show_mistakes(self):
         for widget in self.root.winfo_children():
             widget.destroy()
+
+        label_frame = tk.Frame(self.root)
+        label_frame.pack()
+
+        qdb = self.test_runner.question_database
+        for question_id in self.test_runner.question_database.keys():
+            if not self.test_runner.validate_answer(question_id):
+                question_frame = tk.Frame(label_frame)
+                question_frame.pack()
+
+                label = tk.Label(question_frame, text=f"{question_id}. {qdb[question_id].title}", font=self.font)
+                label.pack(pady=5, padx=5)
+
+                options_frame = tk.Frame(question_frame)
+                options_frame.pack()
+
+                for option in qdb[question_id].options:
+                    highlight = "black"
+                    if self.__is_checked(question_id, option):
+                        highlight = "green" if option.id in self.test_runner.question_database[question_id].correct_option_ids else "red"
+                    label = tk.Label(
+                        options_frame,
+                        text=option.text,
+                        font=self.font,
+                        fg=highlight)
+                    label.pack(pady=5, padx=5)
