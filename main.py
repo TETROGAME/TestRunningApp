ad7955dbaf73e1d66b8227047334efb0194467e8
Тимошенко Никита <minecrosoft231231@gmail.com>
2026-01-29 22:11:01 +0300

    Divided example files into end and ru

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from TestRunner import TestRunner
 from QuizApplication import QuizApplication as QApp
 def main():
-    runner = TestRunner.TestRunner("example_questions.json", 5)
+    runner = TestRunner.TestRunner("example_questions_ru.json", 5)
     root = tk.Tk()
     app = QApp.QuizApplication(root, runner)
     root.mainloop()
