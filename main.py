c6be3f0f9ec24a8f3d7bb1e6b379740a9ea940e4
Nikita Timoshenko <minecrosoft231231@gmai.com>
2026-01-30 20:23:36 +0300

    Add resource path finding logic for proper work with pyinstaller

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import tkinter as tk
 from TestRunner import TestRunner
 from QuizApplication import QuizApplication as QApp
+import os, sys
+def get_resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
 def main():
-    runner = TestRunner.TestRunner("example_questions_ru.json", 5)
+    relative_file_path = "example_questions_ru.json"
+    resource_path = str(get_resource_path(relative_file_path))
+    runner = TestRunner.TestRunner(resource_path, 5)
     root = tk.Tk()
     app = QApp.QuizApplication(root, runner)
     root.mainloop()
